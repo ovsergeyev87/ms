7e4f512e0ddc326099f05f0a247ede2f36107334
ovsergeyev <o.v.sergeyev@gmail.com>
2016-12-17 13:29:42 -0500

    two

--- my_script2.sh
+++ my_script2.sh
@@ -2,4 +2,4 @@ git clone https://github.com/ovsergeyev/snippets.git
 cp -R ~/snippets/* ~/.vim/bundle/vim-snippets/
 git clone https://github.com/ovsergeyev/mysnippets.git
 cp -R ~/mysnippets/* ~/.vim/bundle/vim-snippets/snippets/
-sudo rm -R Vundle.vim home mysnippets
+sudo rm -R Vundle.vim home mysnippets snippets
